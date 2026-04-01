13d1dc31d4c202f5c2f0021cc96498d907b4305d
devjihad <mdhajihad609@gmail.com>
2026-04-01 08:59:05 +0600

    industry section done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import Hero from './Component/Home/Hero';
 import Threecard from './Component/Home/Threecard';
+import Industry from './Component/Home/Industry';
 
 const App = () => {
   return (
     <div className='font-sans'>    
       <Hero/>
       <Threecard/>
+      <Industry/>
     </div>
   );
 };
---
+++ src/Component/Home/Industry.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import user1 from '../../assets/user1.png'
+import user2 from '../../assets/user2.png'
+import user3 from '../../assets/user3.png'
+import review from '../../assets/Ratting.png'
+const Industry = () => {
+    return (
+        <div className='container'>
+            <div className='text-center mt-20 mb-12'>
+                <h3 className='text-4xl font-bold mb-3'>Industry</h3>
+                <p className="text-[14px] text-gray-500">Problems trying to resolve the conflict between <br />the two major realms of Classical physics: Newtonian mechanics </p>
+            </div>
+
+            <div className='flex justify-center gap-8'>
+                <div  className='w-75 px-8 py-8 rounded-lg bg-gray-100 flex flex-col items-center gap-3'>
+                    <img src={user1} alt="" className='w-30' />
+                    <p className="text-[14px] text-gray-500 text-center">Slate helps you see how many more days you need to work to reach your financial goal for the month and year.</p>
+                    <div className='flex gap-2'>
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                    </div>
+
+                </div>
+                <div className='w-75 px-8 py-8 rounded-lg bg-gray-100 flex flex-col items-center gap-3'>
+                    <img src={user2} alt=""  className='w-30' />
+                    <p  className="text-[14px] text-gray-500 text-center">Slate helps you see how many more days you need to work to reach your financial goal for the month and year.</p>
+                    <div className='flex gap-2'>
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                    </div>
+
+                </div>
+                <div className='w-75 px-8 py-8 rounded-lg bg-gray-100 flex flex-col items-center gap-3'>
+                    <img src={user3} alt=""  className='w-30'  />
+                    <p className="text-[14px] text-gray-500 text-center">Slate helps you see how many more days you need to work to reach your financial goal for the month and year.</p>
+                    <div className='flex gap-2'>
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                        <img src={review} alt="" />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Industry;
