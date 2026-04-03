e954789595fa1dc4f0a76edc96115b93ca50bc01
devjihad <mdhajihad609@gmail.com>
2026-04-03 09:11:57 +0600

    booking section done

--- src/Component/Home/Apointment.jsx
+++ src/Component/Home/Apointment.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import rightbg from '../../assets/rightbg.png'
+import right from '../../assets/media.png'
 const Apointment = () => {
     return (
-        <div className='bg-gray-900 mt-20'>
-            <div className=' container  min-h-screen'>
+        <div className='bg-gray-900 mt-20 relative'>
+            <div className=' container  min-h-200'>
             <div className='py-16 flex justify-between items-center text-white'>
                 <div >
                     <h3 className='text-4xl font-semibold mb-3'>We Have Branches All <br />Over The World</h3>
@@ -13,6 +14,41 @@ const Apointment = () => {
                 <img src={rightbg} alt="" />
             </div>
              </div>
+
+           <div className='absolute top-120 right-1/2 translate-x-1/2'>
+             <div className=' bg-white w-210 flex rounded-lg '>
+                <div className='w-1/3 '>
+                    <img src={right} alt="" />
+                </div>
+                <div className='w-2/3 mx-8'>
+                    <h4 className='text-3xl font-bold text-center my-10 '>Book Apoinment</h4>
+                    <div className='flex flex-col gap-2.5 mb-2.5 '>
+                        <label htmlFor="" className='text-[14px] font-semibold'>Name*</label>
+                        <input type="text" placeholder='Full Name' className='py-2 px-5 border border-gray-500 rounded-lg outline-0 bg-gray-200 text-[16px] font-semibold'/>
+                    </div>
+
+                    <div className='flex flex-col gap-2.5 mb-2.5'>
+                        <label htmlFor="" className='text-[14px] font-semibold'>Email*</label>
+                        <input type="text" placeholder='Enter Email' className='py-2 px-5 border border-gray-500 rounded-lg outline-0 bg-gray-200 text-[16px] font-semibold'/>
+                    </div>
+
+                    <div className='flex flex-col gap-2.5 mb-2.5'>
+                        <label htmlFor="" className='text-[14px] font-semibold'>Depertment*</label>
+                        <input type="text" placeholder='Enter Depertment' className='py-2 px-5 border border-gray-500 rounded-lg outline-0 bg-gray-200 text-[16px] font-semibold'/>
+                    </div>
+
+                    <div className='flex flex-col gap-2.5 mb-10'>
+                        <label htmlFor="" className='text-[14px] font-semibold'>Time*</label>
+                        <input type="text" placeholder='Enter your time' className='py-2 px-5 border border-gray-500 rounded-lg outline-0 bg-gray-200 text-[16px] font-semibold'/>
+                    </div>
+
+                    <button className="px-5 py-2 w-full text-center  border border-gray-400 rounded-xl font-semibold   text-[16px] hover:bg-button hover:text-white "> Book Apoinment</button>
+                   
+                   
+                </div>
+            </div>
+           </div>
+
         </div>
     );
 };
